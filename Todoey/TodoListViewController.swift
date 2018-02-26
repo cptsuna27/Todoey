145cead2b39de38482d3b143844a6dac2668075f
Sherome Hardison <CPtsuna@sheromes-mbp.attlocal.net>
2018-02-25 23:46:09 -0500

    Add user default

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -13,9 +13,15 @@ class TodoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let items = defaults.array(forKey: "TodoListArray") as! [String] {
+            itemArray = items
+        }
     }
 
     //MARK - Tableview DataSource Methods
@@ -60,6 +66,8 @@ class TodoListViewController: UITableViewController {
             //what will happen once the user clicks the add item button
             
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             self.tableView.reloadData()
         }
